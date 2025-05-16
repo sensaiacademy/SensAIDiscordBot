eae0bdb80974ace05b34780a196f5b64362b6079
wyszogrodzkivideo <tools@wyszogrodzki.video>
2025-05-16 16:15:00 +0200

    Final cleanup of index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ require('dotenv').config();
 const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
 const axios = require('axios');
 
-// Token bota z Discord Developer Portal
-// const DISCORD_BOT_TOKEN = 'MTMwMTE2ODQwMTA3MDE2MjA0Mg.GBcg6i.1926luSXWaroPJNusI3ycPv2CyZmem7EjY6_wM';
-
-// Adres webhooka w n8n (typ POST)
-// const N8N_WEBHOOK_URL = 'https://n8n.feniksfilm.pl/webhook/0893867c-6f54-4ae9-b38b-8f33ed7a4a83';
-
-// Utworzenie klienta Discord z odpowiednimi "intents"
-
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
